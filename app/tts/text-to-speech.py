1a3004124a0787c0b5c0a5a5df1c85c51c469980
Caio Fernandes <caio.fernandes@fit-tecnologia.org.br>
2025-02-06 11:12:12 -0400

    added tts file

---
+++ app/tts/text-to-speech.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.tools import ElevenLabsText2SpeechTool
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print(os.environ["ELEVENLABS_API_KEY"])
+    text_to_speak = "Hello world! I am the real slim shady"
+
+    # tts = ElevenLabsText2SpeechTool()
+    # print(tts.name)
+    # speech_file = tts.run(text_to_speak)
+    # tts.play(speech_file)
+
+    client = ElevenLabs(
+        api_key=os.environ["ELEVENLABS_API_KEY"],
+    )
+    audio = client.text_to_speech.convert(
+        text=text_to_speak,
+        voice_id="JBFqnCBsd6RMkjVDRZzb",
+        model_id="eleven_multilingual_v2",
+        output_format="mp3_44100_128",
+    )
+    # play(audio)
